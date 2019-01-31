b3a84e4f5250640f970f9f357c057f6f58ba0c6a
Przemyslaw Baran <przemyslaw.baran09@gmail.com>
2019-01-31 22:49:47 +0100

    E8, printing vector horizontaly with borders

--- CppTutorial/CppTutorial.cpp
+++ CppTutorial/CppTutorial.cpp
@@ -8,31 +8,44 @@
 #include <ctime>
 #include <string>
 
-void Bubble(std::vector<int>& valueVec);
+void PrintHorizontalyVec(std::vector<int> vec);
 
 int main()
 {
 	
-	std::vector<int> toSort{ 1, 10, 5, 3, 34, 6, 2, 11, 15, 36, 2 };
-	Bubble(toSort);
-	for (auto val : toSort) {
-		std::cout << val << " " << std::endl;
-	}
+	std::vector<int> theVec { 1, 10, 5, 3, 34 };
+	PrintHorizontalyVec(theVec);
 
 	return 0;
 }
 
-void Bubble(std::vector<int>& valueVec) {
-
-	int k = 1;
-	for (int i = 0; i < valueVec.size() - 1; i++) {
-		for (int j = 0; j < valueVec.size() - k; j++) {
-			if (valueVec[j] > valueVec[j + 1]) {
-				int temp = valueVec[j + 1];
-				valueVec[j + 1] = valueVec[j];
-				valueVec[j] = temp;
-			}
+void PrintHorizontalyVec(std::vector<int> vec) {
+	int numOfSpaces = 0;
+	for (int i = 0; i < vec.size() * 4; i++) {
+		std::cout << "-";
+	}
+	std::cout << std::endl;
+	std::cout << "|";
+	for (int i = 0; i < vec.size(); i++) {
+		std::cout << " " << i ;
+		if (vec[i] >= 10) {
+			std::cout << "  |";
+			numOfSpaces++;
 		}
-		k++;
+		else
+			std::cout << " |";
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < vec.size() * 4 + numOfSpaces; i++) {
+		std::cout << "-";
+	}
+	std::cout << std::endl;
+	std::cout << "|";
+	for (int i = 0; i < vec.size(); i++) {
+		std::cout << " " << vec[i] << " |";
+	}
+	std::cout << std::endl;
+	for (int i = 0; i < vec.size() * 4 + numOfSpaces; i++) {
+		std::cout << "-";
 	}
 }
